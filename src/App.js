d53415f7f84e671fc1aa336506580be959dd0370
kevincpan <kevincpan@gmail.com>
2020-04-21 14:24:06 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const url =
+        'https://s3-us-west-2.amazonaws.com/tecton.ai.public/coding_exercise_1/tables.json';
+      const request = await fetch(url);
+      const data = await request.json();
+      console.log(data);
+    })();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
